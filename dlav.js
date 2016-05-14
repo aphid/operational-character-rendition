3b9f2bd24aefe757ba80a27055aa7e5f5abae4c0
abram stern <aphid@ucsc.edu>
2016-05-14 14:25:33 -0700

    changes for writ_large

---
+++ dlav.js
@@ -0,0 +1,147 @@
+/* Reference: https://github.com/cbaatz/damerau-levenshtein.git
+   UNLICENSED */
+/*
+var dict;
+fetch('dict.json').then(function (response) {
+  return response.json()
+}).then(function (json) {
+  dict = json;
+});
+*/
+
+function acompare(a, b) {
+  if (a.distance < b.distance)
+    return -1;
+  else if (a.distance > b.distance)
+    return 1;
+  else
+    return 0;
+}
+
+function hasNumber(myString) {
+  return (
+    /\d/.test(
+      myString));
+}
+
+var compare = function (ocr, dict) {
+  if (!ocr) {
+    return false;
+  }
+  if (hasNumber(ocr) && ocr.length < 3) {
+    return false;
+  }
+  var words = {};
+  var ranked = [];
+  var dictdiv = document.querySelector('#dict');
+  var compdist = Math.round(ocr.length / 3) + 1;
+  //console.log(ocr, compdist);
+  for (var word of dict) {
+    var dist = distance(ocr, word);
+
+    if (dist === 0) {
+      console.log("perfect match");
+      return ({
+        'original': ocr,
+        'low': 0,
+        'words': [{
+          'word': word,
+          'distance': 0
+      }]
+      });
+    } else if (dist < compdist) {
+
+      ranked.push({
+        'word': word,
+        'distance': dist
+      });
+    }
+
+  }
+  var ranked = ranked.sort(acompare);
+  if (ranked.length) {
+    words.original = ocr;
+    var low = ranked[0].dist;
+    var high = ranked[ranked.length - 1];
+    words.low = low;
+    if (words.high > words.low) {
+      words.high = high;
+    }
+    words.words = ranked;
+    return words;
+  }
+}
+
+damerau = true;
+
+function insert(c) {
+  return 1;
+};
+
+function remove(c) {
+  return 1;
+};
+
+function substitute(from, to) {
+  return 1;
+};
+
+function transpose(backward, forward) {
+  return 1;
+};
+
+function distance(down, across) {
+  // http://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
+  var ds = [];
+  if (down === across) {
+    return 0;
+  } else {
+    down = down.split('');
+    down.unshift(null);
+    across = across.split('');
+    across.unshift(null);
+    down.forEach(function (d, i) {
+      if (!ds[i]) ds[i] = [];
+      across.forEach(function (a, j) {
+        if (i === 0 && j === 0) ds[i][j] = 0;
+        // Empty down (i == 0) -> across[1..j] by inserting
+        else if (i === 0) ds[i][j] = ds[i][j - 1] + insert(a);
+        // Down -> empty across (j == 0) by deleting
+        else if (j === 0) ds[i][j] = ds[i - 1][j] + remove(d);
+        else {
+          // Find the least costly operation that turns
+          // the prefix down[1..i] into the prefix
+          // across[1..j] using already calculated costs
+          // for getting to shorter matches.
+          ds[i][j] = Math.min(
+            // Cost of editing down[1..i-1] to
+            // across[1..j] plus cost of deleting
+            // down[i] to get to down[1..i-1].
+            ds[i - 1][j] + remove(d),
+            // Cost of editing down[1..i] to
+            // across[1..j-1] plus cost of inserting
+            // across[j] to get to across[1..j].
+            ds[i][j - 1] + insert(a),
+            // Cost of editing down[1..i-1] to
+            // across[1..j-1] plus cost of
+            // substituting down[i] (d) with across[j]
+            // (a) to get to across[1..j].
+            ds[i - 1][j - 1] + (d === a ? 0 : substitute(d, a))
+          );
+          // Can we match the last two letters of down
+          // with across by transposing them? Cost of
+          // getting from down[i-2] to across[j-2] plus
+          // cost of moving down[i-1] forward and
+          // down[i] backward to match across[j-1..j].
+          if (damerau && i > 1 && j > 1 && down[i - 1] === a && d === across[j - 1]) {
+            ds[i][j] = Math.min(
+              ds[i][j],
+              ds[i - 2][j - 2] + (d === a ? 0 : transpose(d, down[i - 1]))
+            );
+          };
+        };
+      });
+    });
+    return ds[down.length - 1][across.length - 1];
+  };
+};
--- textimony.js
+++ textimony.js
@@ -1,15 +1,139 @@
 var container, full, fullCtx, line, lineCtx, img, words, wat, type, typeCtx;
 var begin, end, linetrack, lines = [],
   text = [],
+  txt,
   ls = [],
+  read,
   lstemp = [],
-  pause, count = 0;
+  busy = false,
+  count = 0;
 var suspect, dict;
 var statement;
-var interval = 125;
+var interval = 103;
+var stahp = false;
+var block = ['-', '.', '`', '--', '='];
+
+var word = function (options) {
+  var wd = this;
+  if (!options.text || (block.indexOf(options.text.trim()) !== -1)) {
+    options.text = "?"
+    options.fail = true;
+  }
+  if (options.text.trim().length === 0) {
+    options.text = "?"
+    options.fail = true;
+  }
+  console.log("word #" + statement.words.length + ": " + options.text);
+  for (var opt in options) {
+    this[opt] = options[opt];
+  }
+  this.potentials = [];
+  this.potpos = 0;
+  return wd;
+
+
+}
+
+word.prototype.draw = function () {
+  var word = this;
+
+  if (busy) {
+    window.setTimeout(function () {
+      word.draw();
+    }, 50);
+  } else {
+    console.log("Drawing!" + this.text);
+    busy = true;
+    if (word.endpos) {
+      read.style.fontSize = "15vh";
+
+      var delay = 1250;
+      var wordw = word.endpos.x - word.pos.x;
+      var wordh = word.endpos.y - word.pos.y;
+      type.width = wordw || 1;
+      type.height = wordh || 1;
+      console.log(wordw, wordh);
+      typeCtx.clearRect(0, 0, type.width, type.height);
+      var post = "";
+      txt.textContent = "";
+      read.textContent = "";
+      var readMsg = "";
+      if (word.potentials.length > 1) {
+        for (var i = 0; i < word.potentials.length; i++) {
+          if (i !== word.potentials.length - 1) {
+            post = "//";
+            read.style.fontSize = "8vh";
+            long = 5000;
+          }
+          read.classList.add('word');
+
+          readMsg = readMsg + word.potentials[i].word + post;
+        }
+      } else {
+        readMsg = word.text;
+      }
+      console.log(type.width, type.height, word.pos.x);
+
+      typeCtx.drawImage(img, word.pos.x, word.pos.y, type.width, type.height, 0, 0, type.width, type.height);
+      read.textContent = readMsg;
+    }
+    window.setTimeout(function () {
+      busy = false;
+    }, delay);
+  }
+};
+
+word.prototype.pots = function () {
+  this.draw();
+
+  if (this.rawResults) {
+    for (var res of this.rawResults) {
+      this.potentials.push({
+        "word": res.word,
+        "distance": res.distance,
+        "type": "dict"
+      });
+    }
+  }
+  if (this.suspResults) {
+    susps = this.suspResults;
+    for (var res of this.suspResults) {
+      this.potentials.push({
+        "word": res.word,
+        "distance": res.distance,
+        "type": "suspicious"
+      });
+    }
+  }
+  if (this.potentials.length > 1) {
+    this.flip();
+  }
+};
+
+word.prototype.flip = function () {
+  var wd = this;
+  var span = this.span;
+  this.potpos++;
+  if (this.potpos >= this.potentials.length) {
+    this.potpos = 0;
+  }
+  var thispot = this.potentials[this.potpos];
+
+  span.textContent = thispot.word + " ";
+  if (thispot.type === "suspicious") {
+    span.style.color = "red";
+  } else {
+    span.style.color = "white";
+  }
+
+  window.setTimeout(function () {
+    wd.flip();
+  }, 300 / thispot.distance + 100)
+
+};
 
 document.addEventListener("DOMContentLoaded", function (event) {
-
+  console.log("setting up");
 
   //set up all the things.  fold thes into the object at some point
   container = document.querySelector('#container');
@@ -21,22 +145,33 @@ document.addEventListener("DOMContentLoaded", function (event) {
   words = document.querySelector('#words');
   var imgData;
   wat = document.querySelector('#wat');
+  txt = document.querySelector('#text');
+  read = document.querySelector('#read');
+
   type = document.querySelector("#type");
   typeCtx = type.getContext("2d");
 
 
   Promise.all([get('dict.json'), get('suspect.json')]).then(function (values) {
-    dict = FuzzySet(JSON.parse(values[0]));
-    suspect = FuzzySet(JSON.parse(values[1]));
+    rawdict = JSON.parse(values[0]);
+    suspectdict = JSON.parse(values[1]);
     document.querySelector('#waiting').textContent = "";
 
+    /*statement = new Doc({
+  pages: ['questionnaire00.jpg', 'questionnaire01.jpg', 'questionnaire02.jpg', 'questionnaire03.jpg', 'page0.jpg', 'page1.jpg', 'page2.jpg'],
+    title: 'Buckley Statement',
+    hearingId: 'asdf'
+});
     statement = new Doc({
-      pages: ['questionnaire00.jpg', 'questionnaire01.jpg', 'questionnaire02.jpg', 'questionnaire03.jpg', 'page0.jpg', 'page1.jpg', 'page2.jpg'],
-      title: 'Buckley Statement',
-      hearingId: 'asdf'
+      pages: ['texts/090521_litt-0.jpg', 'texts/090521_litt-1.jpg', 'texts/090521_litt-2.jpg'],
+      title: 'Litt statement'
     });
 
+    */
+
   });
+
+
 });
 //doc constructinator
 var Doc = function (options) {
@@ -72,7 +207,6 @@ Doc.prototype.init = function () {
   document.querySelector('img').onload = function () {
     full.width = this.width;
     full.height = this.height;
-    this.style.display = "none";
     fullCtx.clearRect(0, 0, full.width, full.height);
     fullCtx.drawImage(this, 0, 0);
     doc.process();
@@ -92,6 +226,7 @@ Doc.prototype.processLines = function () {
   console.log("got " + this.lines.length + " lines, processing")
   for (var i = 0; i < this.lines.length; i++) {
     var line = this.lines[i];
+
     fullCtx.fillStyle = "rgb(0,0,0)";
     //console.dir(line);
     var lineHeight = line.height;
@@ -101,8 +236,11 @@ Doc.prototype.processLines = function () {
         letter.wordEnd = true;
         letter.lineEnd = true;
       }
+      letter.lineHeight = line.height;
       if (letter.height > 100 || letter.width > 200) {
-        console.log(letter);
+        //big letter, idk.
+        this.letters.push(letter);
+
       } else {
         this.letters.push(letter);
       }
@@ -113,89 +251,193 @@ Doc.prototype.processLines = function () {
 
 };
 
+
+
 Doc.prototype.addWord = function (word) {
-  console.log(word.text);
-  this.words.push(word);
-  var span = document.createElement('span');
-  span.textContent = word.text + " ";
-  words.appendChild(span);
+  if (!word || word.fail) {
+    return false;
+  } else {
+    //console.dir(word);
+    var span = document.createElement('span');
+    word.span = span;
+    span.style.fontSize = word.lineHeight * .6 + "px";
+    if (word.text !== "? ") {
+      this.words.push(word);
+    }
+    span.textContent = word.text + " "
+    words.appendChild(span);
 
-  if (word.lineEnd) {
-    words.appendChild(document.createElement('br'));
+    if (word.lineEnd) {
+      span.previousSibling.insertAdjacentHTML('beforeend', "<br/><br/>");
+    }
+
+    words.scrollTop = words.scrollHeight;
+
+    if (word.text.length > 2) {
+
+      var comp = compare(word.text.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ""), rawdict);
+      if (comp) {
+        if (comp.low === 0 || comp.words.length === 1) {
+          span.textContent = comp.words[0].word + " ";
+        } else {
+          span.textContent = comp.words[0].word + " ";
+
+          //span.textContent = span.textContent + JSON.stringify(comp);
+        }
+        word.rawResults = comp.words;
+
+      } else {
+        word.compFailed = true;
+        span.classList.add('iffy');
+      }
+    }
+
+
+    if (word.text.length > 2) {
+
+      word.draw();
+      var comp = compare(word.text.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ""), suspectdict);
+      if (comp) {
+        span.classList.add('suspect');
+        if (comp.low === 0) {
+          span.textContent = comp.words[0].word + " ";
+        } else {
+          span.textContent = comp.words[0].word + " ";
+
+          //span.textContent = span.textContent + JSON.stringify(comp);
+        }
+        word.suspResults = comp.words;
+
+      } else {
+        //word.compFailed = true;
+        //span.classList.add('iffy');
+      }
+
+      word.pots();
+    }
   }
-
-  words.scrollTop = words.scrollHeight;
-  window.setTimeout(function () {
-    var fuzzy = dict.get(word.text);
-    fuzzy = fuzzy[0];
-    if (fuzzy[0] > 0.614) {
-      span.textContent = fuzzy[1] + " ";
-    }
-
-  }, 1000);
-  window.setTimeout(function () {
-    var fuzzy = suspect.get(word.text);
-    fuzzy = fuzzy[0];
-    if (fuzzy[0] > 0.7) {
-      span.classList.add('suspect');
-
-      span.textContent = "*" + fuzzy[1] + "* ";
-    }
-
-  }, 1000);
 };
 
 
+
 Doc.prototype.drawLetters = function () {
+
   var doc = this;
-  var startWord,
-    matches = "";
-  this.dLetters.push(this.letters[this.currentChr]);
-
-  this.currentChr++;
-  if (this.currentChr >= this.letters.length) {
-    this.dLetters = [];
-    this.init();
+  if (stahp) {
+    return false;
   }
+  if (busy) {
+    console.log("busy");
+  } else {
+    var startWord,
+      matches = "";
+    this.dLetters.push(this.letters[this.currentChr]);
 
-  var letter = this.letters[this.currentChr];
-  if (letter.matches.length) {
-    for (var match of letter.matches) {
-
-      matches = matches + match.letter;
+    this.currentChr++;
+    if (this.currentChr >= this.letters.length) {
+      this.dLetters = [];
+      this.init();
     }
-    if (letter.wordEnd || matches.indexOf(' ') !== -1 || matches.indexOf(',') !== -1) {
 
-      if (letter.wordEnd) {
-        console.log("wordend, matches: " + matches);
+    var letter = this.letters[this.currentChr];
+    //console.dir(letter);
+    if (!letter) {
+      console.log('no letter at ' + this.currentChr);
+      return false;
+    }
+    var pct = (letter.y / img.height) - .2;
+    if (pct < 0) {
+      pct = 0;
+    }
+    full.style.top = "-" + (full.clientHeight * pct) +
+      "px";
+
+    if (letter.matches.length) {
+      for (var match of letter.matches) {
+
+        matches = matches + match.letter;
+        //console.log(matches);
+      }
+      if (letter.wordEnd || matches.indexOf(' ') !== -1 || matches.indexOf(',') !== -1) {
+        letter.wordEnd = true;
+
+        if (letter.wordEnd) {
+          console.log("wordend, matches: " + matches);
+          this.word.text = "" + this.word.text + letter.matches[0].letter;
+        }
+        if (letter.lineEnd) {
+          this.word.lineEnd = true;
+        }
+
+        this.word.lineHeight = letter.lineHeight;
+
+        this.word.pos = {
+          "x": this.letters[this.currentChr - (this.word.text.length - 1)].x,
+          "y": this.letters[this.currentChr - (this.word.text.length - 1)].y
+        };
+        var lastlet = this.letters[this.currentChr];
+        this.word.endpos = {
+          "x": lastlet.x + lastlet.width,
+          "y": lastlet.y + lastlet.height
+        };
+
+
+        if (this.word.text == "" || this.word.text == "-") {
+          this.word = {
+            text: ''
+          };
+        } else if (this.word.text.includes('--')) {
+          var idx = this.word.text.indexOf('--');
+          var altWord = JSON.parse(JSON.stringify(this.word));
+          altWord.text = this.word.text.split('--')[1];
+          altWord.pos = {
+            "x": this.letters[this.currentChr - (altWord.text.length - 1)].x,
+            "y": this.letters[this.currentChr - (altWord.text.length - 1)].y
+          };
+          this.word.text = this.word.text.split('--')[0];
+          this.word.endpos = {
+            "x": this.letters[this.currentChr - (this.word.text.length - 1 + idx)].x,
+            "y": this.letters[this.currentChr - (this.word.text.length - 1 + idx)].y
+          };
+          this.addWord(new word(this.word));
+          this.addWord(new word(altWord));
+        } else if (this.word.text.includes(':')) {
+          var altWord = JSON.parse(JSON.stringify(this.word));
+          altWord.text = this.word.text.split(':')[1];
+          this.word.text = this.word.text.split(':')[0];
+          this.addWord(new word(this.word));
+          //ugh need to figure out positioning
+          this.addWord(new word(altWord));
+        } else {
+          this.addWord(new word(this.word));
+        }
+
+        this.word = {
+          text: ''
+        };
+
+      } else {
         this.word.text = "" + this.word.text + letter.matches[0].letter;
       }
-      if (letter.lineEnd) {
-        this.word.lineEnd = true;
-      }
-      this.addWord(this.word);
-      this.word = {
-        text: ''
-      };
+      fullCtx.fillRect(letter.x, letter.y, letter.width, letter.height);
+      fullCtx.fillRect(letter.x, letter.y, letter.width, letter.height);
+      var pct = (letter.y / img.height) - .2;
+      full.style.top = "-" + (full.clientHeight * pct) +
+        "px";
+      //full.style.top = "-" + (letter.y - 430) + "px";
 
+      //full.style.left = "-" + (letter.x - 130) + "px";
+      type.width = letter.width;
+      type.height = letter.height;
+      typeCtx.clearRect(0, 0, type.width, type.height);
+
+      typeCtx.drawImage(img, letter.x, letter.y, letter.width, letter.height, 0, 0, type.width, type.height);
+      txt.textContent = matches;
     } else {
-      this.word.text = "" + this.word.text + letter.matches[0].letter;
+      txt.textContent = "???";
     }
-    fullCtx.fillRect(letter.x, letter.y, letter.width, letter.height);
-    fullCtx.fillRect(letter.x, letter.y, letter.width, letter.height);
-    full.style.top = "-" + (letter.y - 130) + "px";
-    full.style.left = "-" + (letter.x - 130) + "px";
-    type.width = letter.width;
-    type.height = letter.height;
-    typeCtx.clearRect(0, 0, type.width, type.height);
 
-    typeCtx.drawImage(img, letter.x, letter.y, letter.width, letter.height, 0, 0, type.width, type.height);
-    document.querySelector('#text').textContent = matches;
-  } else {
-    document.querySelector('#text').textContent = "???";
   }
-
-
 
   if (this.dLetters.length === this.letters.length) {
     console.log('done');
@@ -283,4 +525,10 @@ function get(url) {
     // Make the request
     req.send();
   });
+}
+
+function hasNumber(myString) {
+  return (
+    /\d/.test(
+      myString));
 }
